c119c9f467d52d65c23e76d28646e7274e31a4f4
TANIGAWA Takashi <mihailjp@gmail.com>
2013-02-08 23:47:14 +0900

    hl_newstateの改良

--- commands.cpp
+++ commands.cpp
@@ -4,12 +4,21 @@
 #include "hsp/hsp3plugin.h"
 
 void hsplua_cmd::hl_newstate() { // 新規Luaステート
-	int statNum = exinfo->HspFunc_prm_getdi(luaStates.size()); // ステート管理番号
+	int statNum = exinfo->HspFunc_prm_getdi(-1); // ステート管理番号
+	if (statNum == -1) {
+		statNum = luaStates.size();
+		for (int i = 0; i < luaStates.size(); ++i) {
+			if (luaStates[statNum] == NULL) {
+				statNum = i; break;
+			}
+		}
+	}
 	if (statNum < 0) throw HSPERR_ILLEGAL_FUNCTION; // マイナスを指定してた場合
 	if (luaStates.size() <= statNum) luaStates.resize(statNum + 1, NULL);
 	luaStates[statNum] = luaL_newstate(); // 新規ステート
 	if (luaStates[statNum] == NULL) throw HSPERR_OUT_OF_MEMORY;
 	currentState = statNum;
+	stat = statNum;
 	return;
 }
 
